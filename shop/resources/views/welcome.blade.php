9c7b042a3ce64184994af4467dfbc2c6f43d2cda
azhan10 <rashid.azhan6@hotmail.co.uk>
2016-10-19 14:52:15 +0100

    Welcome Interface improvement
    I improved the Welcome page interface

--- shop/resources/views/welcome.blade.php
+++ shop/resources/views/welcome.blade.php
@@ -5,13 +5,22 @@
 
 @section('content')
 
+<img src="http://www.cotuitcenterforthearts.org/images/bbevents/CCftA_Film_Loop.png" width="100%">
 
 
+<br/>
+<br/>
 
-<h1>Welcome</h1>
 
+<div class="panel panel-default">
+  <div class="panel-body text-center">
 
-<a href="{{ route('FilmCRUD.index') }}">View current films</a>
+  <h1>Welcome to film review</h1>
+<a class="btn btn-info" href="{{ route('FilmCRUD.index') }}">View current films</a>
+
+  </div>
+</div>
+
 
 
 
