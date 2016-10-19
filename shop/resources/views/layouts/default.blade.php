951835a046b9c6e4a4de971b581595c5b69d1df2
azhan10 <rashid.azhan6@hotmail.co.uk>
2016-10-19 11:45:13 +0100

    Layout Design
    So far, i structured the layout design

---
+++ shop/resources/views/layouts/default.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script>
+
+
+    <title>Laravel CRUD</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+
+ 
+
+
+<div class="container">
+
+    @yield('content')
+
+</div>
+
+ 
+
+
+</body>
+
+</html>
+
